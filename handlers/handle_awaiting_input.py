712bf06837abbb19f249a308a2f76486dee50c24
Neytol <elusive142@gmail.com>
2025-11-03 22:34:21 +0300

    refactor

--- handlers/handle_awaiting_input.py
+++ handlers/handle_awaiting_input.py
@@ -7,10 +7,10 @@ from handlers.show_main_buttons import show_main_buttons
 
 async def handle_awaiting_input(update: Update, context: ContextTypes.DEFAULT_TYPE, user_text: str):
     if context.user_data.get("awaiting_weather"):
-        context.user_data["awaiting_weather"] = False
+        del context.user_data['awaiting_weather']
         result = await get_weather(user_text)
-    elif context.user_data.get("awaiting_currency"):
-        context.user_data["awaiting_currency"] = False
+    elif context.user_data.get('awaiting_currency'):
+        del context.user_data['awaiting_currency']
         result = await get_currency(user_text)
     else:
         return False
--- handlers/handle_message.py
+++ handlers/handle_message.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from database import increment_message_count, get_all_users, register_user
+from database import increment_message_count, get_all_users, register_user, get_user
 from handlers.handle_awaiting_input import handle_awaiting_input
 from handlers.start_command import start_command
 from logger import logger
@@ -9,17 +9,13 @@ from logger import logger
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
     user_text = update.message.text
-    users = await get_all_users()
-    try:
-        if len(users) == 0:
-            await start_command(update, context)
-            return
-        for u in users:
-            if user.id != u['id']:
-                await start_command(update,context)
-                return
 
-        if await handle_awaiting_input(update, context, user_text):
+    if await handle_awaiting_input(update, context, user_text):
+        return
+    try:
+        user_data = await get_user(user.id)
+        if user_data is None:
+            await start_command(update,context)
             return
         await increment_message_count(user.id)
         await update.message.reply_text("Используй /start")
