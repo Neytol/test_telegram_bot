d47a48ea7fcb74c3cb0cf228a2e09b4291a4c8de
Neytol <elusive142@gmail.com>
2025-11-05 23:17:02 +0300

    feat: users_info

---
+++ handlers/users_info.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from database import get_all_users
+
+
+async def users_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    users = await get_all_users()
+    for u in users:
+        await update.message.reply_text(f"""
+            "id": {u['id']},
+            "username": {u['username']},
+            "first_name": {u['first_name']},
+            "message_count": {u['message_count']},
+            "registered": {u['registered']},
+            "last_activity": {u['last_activity']}
+        """)
